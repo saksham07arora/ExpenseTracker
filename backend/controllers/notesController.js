eae304a7005512545155911f4f4f9adcf867b364
Sudiksha gandhi <86057281+Sudiksha07@users.noreply.github.com>
2024-09-18 01:03:21 +0530

    Delete backend directory

--- backend/controllers/notesController.js
+++
@@ -1,8 +0,0 @@
-const saveNote = (req, res) => {
-    const { note } = req.body;
-  
-    res.status(201).send('Note saved');
-  };
-  
-  module.exports = { saveNote };
-  
--- backend/models/noteModels.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const noteSchema = new mongoose.Schema({
-  note: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Note', noteSchema);
